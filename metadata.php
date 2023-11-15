498d414a4296d883aee86251904a3242516ea450
Radio Tiempo Final <149134624+RadioTiempoFinal@users.noreply.github.com>
2023-11-15 17:38:40 -0600

    Update metadata.php

--- metadata.php
+++ metadata.php
@@ -1,4 +1,4 @@
-metadata<?php
+https://zenoplay.zenomedia.com/api/zenofm/nowplaying/6ss1gdc7uv8uv<?php
 /////////////////////////////////////////////////////
 // READ META DATA FROM STREAMING URL
 //
